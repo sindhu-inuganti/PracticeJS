bf83dc9c178f2587355d356d158b99072b5f9e08
sindhu-inuganti <60869407+sindhu-inuganti@users.noreply.github.com>
2020-02-24 11:33:55 +0530

    Update tryFunctions.js
    Call, apply and bind

--- tryFunctions.js
+++ tryFunctions.js
@@ -13,3 +13,9 @@ function myFunc(a=10,b=10)
 // console.log(myFunc(4,5))
 // console.log(myFunc(4,5,6))
 
+var data= {x:15,y:15}
+console.log(funcVar.call(data,4,5));//call
+
+var arr=[2,3]
+console.log(funcVar.apply(data,arr))//apply
+console.log(funcVar.apply(data,[4,5]))//apply
